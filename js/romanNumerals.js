58bea66a655d570790f50b3cd0b960ffcb9bbd97
Skyyyr <emuswgluvr@gmail.com>
2022-05-24 15:57:21 -0700

    Roman Numerals

--- js/romanNumerals.js
+++ js/romanNumerals.js
@@ -1,4 +1,35 @@
+/*
+# 1. Write a method TO_ROMAN, TO_ROMAN takes in INPUT_NUMBER (an arabic number)
+# 2. Create a OUTPUT string, set to ''
+# 3. Create a ROMAN_NUMERAL_TO_ARABIC_MAP that includes roman numerals as keys, arabic numbers as values
+# 4. Iterate over ROMAN_NUMERAL_TO_ARABIC_MAP, keep track of ROMAN_NUMERAL and ARABIC_NUMBER
+# 5. Set EVENLY_DIVISIBLE_TIMES = INPUT_NUMBER / ARABIC_NUMBER:
+# 6. If EVENLY_DIVISIBLE_TIMES >= 1
+  # 6a. Append ROMAN_NUMERAL to OUTPUT EVENLY_DIVISIBLE_TIMES
+  # 6b. Subtract ARABIC_NUMBER from INPUT_NUMBER EVENLY_DIVISIBLE_TIMES
+# 7. Return OUTPUT
+*/
 exports.toRoman = function(num) {
-    //First
-
+    const numeralMap = {
+        'M': 1000,
+        'CM': 900,
+        'D': 500,
+        'CD': 400,
+        'C': 100,
+        'XC': 90,
+        'L': 50,
+        'XL': 40,
+        'X': 10,
+        'V': 5,
+        'IV': 4,
+        'I': 1,
+    }
+    let output = '';
+    for (let i in numeralMap) {
+        while (num >= numeralMap[i]) {
+            output += i;
+            num -= numeralMap[i];
+        }
+    }
+    return output;
 };
