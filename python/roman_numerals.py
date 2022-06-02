553745edac030324bcad3b7c5e2e310954f82df8
Skyyyr <emuswgluvr@gmail.com>
2022-06-01 17:01:37 -0700

    Added more notes

--- python/roman_numerals.py
+++ python/roman_numerals.py
@@ -1,5 +1,5 @@
 def to_roman(num):
-    # write your code here!
+    # We mapped out the roman numerals that are possible
     numeralMap = {
         'M': 1000,
         'CM': 900,
@@ -14,9 +14,15 @@ def to_roman(num):
         'IV': 4,
         'I': 1,
     }
+    # Initialized our answer so we can append to it
     output = ''
+    # Loop over the map
     for k in numeralMap:
+        # If the value at the current index is less than or equal to the number given
         while num >= numeralMap[k]:
+            # Add the key to the output (our roman numeral)
+            # Subtract the value from the given number by the value from the current index
             output += k
             num -= numeralMap[k]
+    # We looped through the entire map and converted, so return the output
     return output
